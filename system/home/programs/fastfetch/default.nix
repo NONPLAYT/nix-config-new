5dab823e17998600ce28efc08e0bc0a74630e23c
NONPLAY <76615486+NONPLAYT@users.noreply.github.com>
2026-01-28 21:58:16 +0300

    refactor: update fastfetch

--- system/home/programs/fastfetch/default.nix
+++ system/home/programs/fastfetch/default.nix
@@ -4,109 +4,37 @@
     enable = true;
     
     settings = {
-      logo = {
-        type = "none";
-      };
-      display = {
-        separator = "->   ";
-      };
+      "$schema" = "https://github.com/fastfetch-cli/fastfetch/raw/master/doc/json_schema.json";
       modules = [
-        {
-          type = "title";
-          format = "{6}{7}{8}";
-        }
+        "title"
+        "separator"
+        "os"
+        "host"
+        "kernel"
+        "uptime"
+        "packages"
+        "shell"
+        "display"
+        "de"
+        "wm"
+        "wmtheme"
+        "theme"
+        "icons"
+        "font"
+        "cursor"
+        "terminal"
+        "terminalfont"
+        "cpu"
+        "gpu"
+        "memory"
+        "swap"
+        "disk"
+        "localip"
+        "battery"
+        "poweradapter"
+        "locale"
         "break"
-        {
-          type = "custom";
-          format = "┌───────────────────────────── System Information ─────────────────────────────┐";
-        }
-        "break"
-        {
-          key = "     OS           ";
-          keyColor = "red";
-          type = "os";
-        }
-        {
-          key = "    󰌢 Machine      ";
-          keyColor = "green";
-          type = "host";
-        }
-        {
-          key = "     Kernel       ";
-          keyColor = "magenta";
-          type = "kernel";
-        }
-        {
-          key = "    󰏖 Packages     ";
-          type = "packages";
-        }
-        {
-          key = "    󰅐 Uptime       ";
-          keyColor = "red";
-          type = "uptime";
-        }
-        {
-          key = "    󰍹 Resolution   ";
-          keyColor = "yellow";
-          type = "display";
-          compactType = "original-with-refresh-rate";
-        }
-        {
-          key = "     WM           ";
-          keyColor = "blue";
-          type = "wm";
-        }
-        {
-          key = "     DE           ";
-          keyColor = "green";
-          type = "de";
-        }
-        {
-          key = "     Shell        ";
-          keyColor = "cyan";
-          type = "shell";
-        }
-        {
-          key = "     Terminal     ";
-          keyColor = "red";
-          type = "terminal";
-        }
-        {
-          key = "    󰻠 CPU          ";
-          keyColor = "yellow";
-          type = "cpu";
-        }
-        {
-          key = "    󰍛 GPU          ";
-          keyColor = "blue";
-          type = "gpu";
-        }
-        {
-          key = "    󰑭 Memory       ";
-          keyColor = "magenta";
-          type = "memory";
-        }
-        {
-          key = "    󰩟 Local IP     ";
-          keyColor = "red";
-          type = "localip";
-        }
-        {
-          key = "    󰩠 Public IP    ";
-          keyColor = "cyan";
-          type = "publicip";
-        }
-        "break"
-        {
-          type = "custom";
-          format = "└──────────────────────────────────────────────────────────────────────────────┘";
-        }
-        "break"
-        {
-          paddingLeft = 34;
-          symbol = "circle";
-          type = "colors";
-        }
+        "colors"
       ];
     };
   };
